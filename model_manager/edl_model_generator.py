78d50e010b2a92693037ff8d25f1f0f96be6c913
Julia Gonski <julia.gonski@cern.ch>
2025-06-17 06:24:22 -0700

    EDL model

--- model_manager/edl_model_generator.py
+++ model_manager/edl_model_generator.py
@@ -1,33 +1,58 @@
 from tensorflow.keras import Model
 from tensorflow.keras import layers
 import tensorflow as tf
+import tensorflow_probability as tfp
 from model_evaluation import loss_calculator
 
 
 class EDLModel(Model):
-    def __init__(self, train_config):
-        super(VaeModel, self).__init__()
+    """
+    This model replaces the existing VAe parameters with the Dirichlet Paramaters
 
-        #TODO 
+    """
+    def __init__(self, train_config):
+        super(EDLModel, self).__init__()
+        self.input_shape = (train_config["encoder_design"]["input_layer_dimension"],)
+        self.latent_dim = 2 # This is fixed since we have just two classes
+        self.train_config = train_config
+
+        # Encoder
+        self.encoder_input = layers.Input(shape=self.input_shape)
+        dimensions = train_config["encoder_design"]["inner_layer_dimensions"]
+        x = self.encoder_input
+        for dim in dimensions:
+            self.encoder_x = layers.Dense(dim, activation='relu')(x)
+            x = self.encoder_x
+        self.alpha = layers.Dense(self.latent_dim, activation='softplus')(self.encoder_x)
+        self.z = Sampling()(self.alpha)
+
+        # Decoder
+        self.decoder_input = layers.Input(shape=(self.latent_dim,))
+        op_layer_dimension_list = train_config["decoder_design"]["inner_layer_dimensions"]
+        x = self.decoder_input
+        for dim in op_layer_dimension_list:
+            self.decoder_x = layers.Dense(dim, activation='relu')(x)
+            x = self.decoder_x
+        self.decoder_output = layers.Dense(self.input_shape[0], activation='relu')(self.decoder_x)
+
+        self.encoder = Model(self.encoder_input, [self.z, self.alpha])
+        self.decoder = Model(self.decoder_input, self.decoder_output)
 
     def call(self, inputs):
-        z_mean, z_log_var, z = self.encoder(inputs)
+        alpha, z = self.encoder(inputs)
+        kl_loss = loss_calculator.get_dvae_kl_loss(alpha)
+        strength = self.train_config["hyperparameters"]["kl_strength"]
         reconstructed = self.decoder(z)
         mse = tf.keras.losses.MeanSquaredError()
         reconstruction_loss = mse(inputs, reconstructed) * self.input_shape[0]
-        # Source : https://keras.io/examples/generative/vae/
-        kl_loss = loss_calculator.get_vae_kl_loss(z_log_var, z_mean)
-        total_loss = reconstruction_loss + kl_loss
+
+        total_loss = reconstruction_loss + strength * kl_loss
         self.add_loss(total_loss)
         return reconstructed
 
 
 class Sampling(layers.Layer):
-    """Uses the  basic z_mean, z_log_var to sample"""
+    """Uses the alpha dirichlet to sample"""
 
     def call(self, inputs):
-        z_mean, z_log_var = inputs
-        batch = tf.shape(z_mean)[0]
-        dim = tf.shape(z_mean)[1]
-        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+        return tfp.distributions.Dirichlet(inputs).sample()
