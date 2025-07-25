produced KL and MSE loss histograms successfully, also moved alot of code into seperate methods for better readability and organization + some other minor bug fixes

grapher/graph_generator.py
@@ -7,18 +7,20 @@ import tensorflow as tf
 import keras
 
 
-def generate_loss_vs_epoch(train_config, history):
+def generate_loss_vs_epoch(train_config, history, model_type):
     loss = history.history["loss"]
     val_loss = history.history["val_loss"]
     plt.xlabel('Epoch')
-    plt.ylabel('Loss (MSE)')
+    plt.ylabel('Loss (MSE) (Log)')
     plt.title("Loss vs epoch curve for " + train_config["model_files"]["model_name"])
     plt.xticks(np.arange(0,21,2))
     plt.plot(np.linspace(0, train_config["hyperparameters"]["epochs"], train_config["hyperparameters"]["epochs"]), loss)
     plt.plot(np.linspace(0, train_config["hyperparameters"]["epochs"], train_config["hyperparameters"]["epochs"]), val_loss)
-
+    if model_type == 'EDL' or model_type =='VAE':
+        plt.yscale('log')
+    plt.legend(loc='upper right')
     plt.savefig(os.path.join(train_config["outputs"]["graph_directory"], datetime.datetime.now().strftime(
-        train_config["outputs"]["date_time_format"]) + "_LossVsEpoch.png"), format="png", bbox_inches="tight")
+        train_config["outputs"]["date_time_format"]) + "_" + model_type + "_LossVsEpoch.png"), format="png", bbox_inches="tight")
     plt.show()
 
 

grapher/loss_graph_generator.py
@@ -41,14 +41,14 @@ def generate_roc_curve(train_config, background_loss, signal_loss, labels):
         train_config["outputs"]["date_time_format"]) + "_ROC.png"), format="png", bbox_inches="tight")
     plt.show()
 
-def generate_loss_vs_epoch_curve(train_config, loss, val_loss):
+def generate_loss_vs_epoch_curve(train_config, loss, val_loss, model_type):
     plt.xlabel('Epoch')
     plt.ylabel('Loss (MSE)')
-    plt.title("Loss vs epoch curve for " + train_config["model_files"]["model_name"])
+    plt.title("Loss vs epoch curve for " train_config["model_files"]["model_name"] + "_" + model_type)
     plt.xticks(np.arange(0,21,2))
     plt.plot(np.linspace(0, train_config["hyperparameters"]["epochs"], train_config["hyperparameters"]["epochs"]), loss)
     plt.plot(np.linspace(0, train_config["hyperparameters"]["epochs"], train_config["hyperparameters"]["epochs"]), val_loss)
 
     plt.savefig(os.path.join(train_config["outputs"]["graph_directory"], datetime.datetime.now().strftime(
-        train_config["outputs"]["date_time_format"]) + "_LossVsEpoch.png"), format="png", bbox_inches="tight")
+        train_config["outputs"]["date_time_format"]) + "_" + model_type "_LossVsEpoch.png"), format="png", bbox_inches="tight")
     plt.show()

model_manager/model_evaluation.py
@@ -1,7 +1,9 @@
 #TODO: port eval and plot making code
 import tensorflow as tf
-import keras
-from keras import layers
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
 
 
 def evaluate_model(train_config, model, data, model_type):
@@ -10,3 +12,34 @@ def evaluate_model(train_config, model, data, model_type):
     else:
         results = model.evaluate(data)
     return results
+
+def calculate_loss(train_config, model, data, model_type):
+
+    #truncate as desired
+    data = data[:train_config["data_split"]["max_data"]]
+
+    #Calculating MSE loss
+    reconstructed_data = model(data).numpy()
+    mse = (reconstructed_data - data) ** 2
+    mse_avg = np.mean(mse, axis=1, keepdims=True)
+
+
+    #Calculate KL loss if applicable
+    if model_type == 'EDL':
+        strength = train_config["hyperparameters"]["kl_strength"]
+        num_classes = model.latent_dim
+        prior_alpha_constant = tf.constant([1.0] * num_classes, dtype=tf.float32)
+
+        alpha, _ = model.encoder(data)
+        prior_alpha_broadcasted = tf.broadcast_to(prior_alpha_constant, tf.shape(alpha))
+        kl_divergences_per_sample = tfp.distributions.Dirichlet(alpha).kl_divergence(
+                                tfp.distributions.Dirichlet(prior_alpha_broadcasted))
+        kl_divergence = (strength * kl_divergences_per_sample).numpy()
+
+        return mse_avg, kl_divergence
+    
+    elif model_type == 'VAE':
+        #TODO
+        print("Not yet implemented...")
+    else:
+        return mse_avg